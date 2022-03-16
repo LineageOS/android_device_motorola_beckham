LineageOS/android_device_motorola_beckham

commit ae0484651985bd1e2278126a2cf91b2af84525d8

beckham: Use PRODUCT_*_SEPOLICY_DIRS to build product sepolicy.
We are restoring the old behavior of BOARD_PLAT_*_SEPOLICY_DIR that
extends system sepolicy. To that end, we use another make variable to
specify product sepolicy.

Bug: 119305624
Test: m selinux_policy
Change-Id: I67636b7aaff2bd5ae5309981f9fa2f8ce51edf6f

BoardConfig.mk
@@ -54,8 +54,8 @@ TARGET_HAS_NO_WLAN_STATS := true
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # SELinux
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy-mods/private
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy-mods/public
+PRODUCT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy-mods/private
+PRODUCT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy-mods/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy-mods/vendor
 
 # inherit from the proprietary version

commit ebcd3c0c814e341d6f1918b2271b7728b73b3c5d

fixup! beckham: Use PRODUCT_*_SEPOLICY_DIRS to build product sepolicy
- typo

Fixes: ae0484651985bd1e2278126a2cf91b2af84525d8

Change-Id: Ibea4360761d578992aa50adb63bed4a424190378

BoardConfig.mk
@@ -54,8 +54,8 @@ TARGET_HAS_NO_WLAN_STATS := true
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # SELinux
-PRODUCT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy-mods/private
-PRODUCT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy-mods/public
+PRODUCT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy-mods/private
+PRODUCT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy-mods/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy-mods/vendor
 
 # inherit from the proprietary version