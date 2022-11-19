369e7aeec5687b67eef0e0951c9f05e5efc30440
Nolen Johnson <johnsonnolen@gmail.com>
2022-11-19 00:27:02 -0500

    beckham: Update fingerprint and add description
    Change-Id: Ie12660a100b4b8003a8f8f714aea3d75f53f69ca

--- lineage_beckham.mk
+++ lineage_beckham.mk
@@ -57,9 +57,10 @@ PRODUCT_DEVICE := beckham
 PRODUCT_NAME := lineage_beckham
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := Moto Z3 Play
-PRODUCT_MANUFACTURER := Motorola
+PRODUCT_MANUFACTURER := motorola
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=beckham
+    PRODUCT_NAME=beckham \
+    PRIVATE_BUILD_DESC="beckham-user 9 PPWS29.131-27-1-15 4d8b92 release-keys"
 
-BUILD_FINGERPRINT := motorola/beckham/beckham:8.1.0/OPWS28.70-56-2/8ad8e:user/release-keys
+BUILD_FINGERPRINT := motorola/beckham/beckham:9/PPWS29.131-27-1-15/4d8b92:user/release-keys
